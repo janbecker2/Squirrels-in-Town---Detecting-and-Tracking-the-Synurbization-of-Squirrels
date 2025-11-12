4f43d6e2925cc9b0d9130287028154a32cfa8fc5
JanB <133894095+job2002@users.noreply.github.com>
2025-11-12 18:09:35 +0100

    Implemting BS and first Yolo test

---
+++ yolo_test.py
@@ -0,0 +1,35 @@
+from ultralytics import YOLO
+import cv2 as cv
+
+
+#model = YOLO('yolo11n.pt') 
+model = YOLO(r"runs/detect/squirrel_yolo11/weights/best.pt")
+
+# Load video
+video_path = r"D:\squirrel_vid_short.mp4"
+cap = cv.VideoCapture(video_path)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    results = model(frame, conf=0.5)
+
+    for r in results:
+        img_with_boxes = r.plot()  # Draw boxes on the frame
+
+        print(f"\n Found {len(r.boxes)} objects in this frame")
+        for i, box in enumerate(r.boxes):
+            class_id = int(box.cls)
+            confidence = float(box.conf)
+            class_name = model.names[class_id]
+            print(f"  {i+1}. {class_name} ({confidence:.1%} confidence)")
+
+        cv.imshow('YOLO Detection', img_with_boxes)
+
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv.destroyAllWindows()
